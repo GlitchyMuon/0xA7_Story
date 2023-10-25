e81da1ceef27e87c37d5039d2c2344654f80d880
Carol Van Hem <carol.vh@outlook.com>
2023-10-25 15:46:43 +0200

    unity update (state and ui)

---
+++ 0xA7_Story/Assets/Scripts/ButtonsSwitchState.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsSwitchState : MonoBehaviour
+{
+    public void Voir()
+    {
+        Debug.Log("choix 1");
+    }
+
+    public void Entendre()
+    {
+        Debug.Log("choix 2");
+    }
+}
